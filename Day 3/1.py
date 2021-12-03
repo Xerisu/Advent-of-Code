40c7d10dfc2c58a7b35e91922f611f7953a5980c
Xerisu <k4s14.kk@gmail.com>
2021-12-03 19:55:40 +0100

    Day 3 (finally...)

---
+++ Day 3/1.py
@@ -0,0 +1,45 @@
+input_file = open("./diagnostic-report.txt","r")
+
+reports = input_file.readlines()
+
+bits = 12
+
+reports_split = []
+
+for report in reports:
+    y = int(report) 
+    x = [int(a) for a in str(y)]
+    reports_split.append(x)
+
+for report in reports_split:
+    while len(report) < 12:
+        report.insert(0, int(0))
+
+
+sum1 = 0
+sum0 = 0
+gamma_list = []
+epsilon_list = []
+
+for i in range(bits):
+    for report in reports_split:
+        if report[i] == 0:
+            sum0 += 1
+        else:
+            sum1 += 1
+    if sum0 > sum1:
+        gamma_list.append(int(0))
+        epsilon_list.append(int(1)) 
+    else:
+        gamma_list.append(int(1)) 
+        epsilon_list.append(int(0))
+    sum1 = 0
+    sum0 = 0
+
+gamma = int("".join([str(x) for x in gamma_list]), 2)
+epsilon = int("".join([str(x) for x in epsilon_list]), 2)
+print(gamma*epsilon)
+
+
+
+input_file.close()
---
+++ Day 3/2.py
@@ -0,0 +1,71 @@
+input_file = open("./diagnostic-report.txt","r")
+
+reports = input_file.readlines()
+
+reports_split = []
+
+for report in reports:
+    y = int(report) 
+    x = [int(a) for a in str(y)]
+    reports_split.append(x)
+
+for report in reports_split:
+    while len(report) < 12:
+        report.insert(0, int(0))
+
+#czemu nie działasz kurwo
+
+sum1 = 0
+sum0 = 0
+oxygen_rating = reports_split
+i = 0
+while len(oxygen_rating) != 1:
+    for report in oxygen_rating:
+        if report[i] == 0:
+            sum0 += 1
+        else:
+            sum1 += 1
+
+    filter = []
+    for oxygen in oxygen_rating:
+        if oxygen[i] == 0 and (sum1 >= sum0):
+            filter.append(oxygen)
+        elif oxygen[i] == 1 and (sum0 > sum1):
+            filter.append(oxygen)
+    oxygen_rating = [x for x in oxygen_rating if x not in filter]
+
+    sum1 = 0
+    sum0 = 0
+    i += 1
+
+
+co2_rating = reports_split
+i = 0
+while len(co2_rating) != 1:
+    for report in co2_rating:
+        if report[i] == 0:
+            sum0 += 1
+        else:
+            sum1 += 1
+
+    filter = []
+    for co2 in co2_rating:
+        if co2[i] == 1 and sum1 >= sum0:
+            filter.append(co2)
+        elif co2[i] == 0 and sum0 > sum1:
+            filter.append(co2)
+    co2_rating = [x for x in co2_rating if x not in filter]
+
+    sum1 = 0
+    sum0 = 0
+    i += 1
+
+oxygen = int("".join([str(x) for x in oxygen_rating[0]]), 2)
+co2 = int("".join([str(x) for x in co2_rating[0]]), 2)
+
+print(oxygen_rating[0], oxygen)
+print (co2_rating[0], co2)
+
+print (co2*oxygen)
+
+input_file.close()
