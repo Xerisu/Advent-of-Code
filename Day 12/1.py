3d332fe80bcbfd2218953717eba05a49efa6aa0c
Xerisu <k4s14.kk@gmail.com>
2021-12-17 23:08:28 +0100

    Day 12 (totally done on Day 12)

---
+++ Day 12/1.py
@@ -0,0 +1,32 @@
+input_file = open("./Day 12/path.txt","r")
+
+paths = input_file.readlines()
+paths = [elem.strip() for elem in paths]
+paths = [elem.split('-') for elem in paths]
+input_file.close()
+
+cave_system = {}
+
+for path in paths:
+    if path[0] not in cave_system:
+        cave_system[path[0]] = set()
+    cave_system[path[0]].add(path[1])
+    if path[1] not in cave_system:
+        cave_system[path[1]] = set()
+    cave_system[path[1]].add(path[0])
+
+def dfs(graph, start, end, visited=None):
+    possible_paths = 0
+    if visited is None:
+        visited = set()
+    if not start.isupper():
+        visited.add(start)
+
+    if start == end:
+        return 1
+    
+    for next in graph[start] - visited:
+        possible_paths += dfs(graph, next, end, visited.copy())
+    return possible_paths
+
+print(dfs(cave_system, 'start', 'end'))
